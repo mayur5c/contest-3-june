adaa0e7bf4737ef213697ebf5655a2638b9fff5f
Mayur Chopra <mayur5c@live.in>
2023-07-01 18:42:06 +0530

    final Changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ import { resetCart } from "../Cart/cartSlice";
 export default function Navbar() {
   let dispatch = useDispatch();
   return (
-    <div className="flex justify-between sticky top-0 z-50 bg-pink-950 text-yellow-100 text-xl items-center p-4 w-full sm:flex-col sm:gap-6 xs:gap-4">
+    <div className="flex justify-between sticky top-0 z-50 bg-blue-950 text-yellow-100 text-xl items-center p-4 w-full sm:flex-col sm:gap-6 xs:gap-4">
       <div name="logo">
         {logo}{" "}
         <span className="align-middle text-4xl font-creepster text-yellow-100 xs:text-3xl">
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/contest-3-june/#",
+  base: "/contest-3-june/",
   plugins: [react()],
 })
