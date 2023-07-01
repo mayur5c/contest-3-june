4d5ba7d662970ae73551fc91399f42a75233bd9a
Mayur Chopra <mayur5c@live.in>
2023-07-01 18:07:38 +0530

    many changes

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/contest-3-june/#",
+  plugins: [react(),tailwindcss()],
+})
